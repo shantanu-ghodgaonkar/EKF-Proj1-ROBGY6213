d190123bac83e5973be2dd9a2a4252a252920068
shantanu-ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2024-03-12 14:01:53 -0400

    Part 2 main and prediction implementation complete, review remaining. Update step implementation remainging

--- code/part2/KalmanFilt_Part2.m
+++ code/part2/KalmanFilt_Part2.m
@@ -9,10 +9,20 @@ savedStates = zeros(15, length(sampledTime)); %J ust for saving state his.
 prevTime = 0; %last time step in real time
 %write your code here calling the pred_step.m and upd_step.m functions
 for i = 1:length(sampledTime)
-
-
+    % MY IMPLEMENTATION START -------------------------------------------------
+    if sampledData(i).is_ready == 1
+        dt = sampledTime(i) - prevTime; % Calculate time interval dt
+        prevTime = sampledTime(i); % Update the previous time variable
+        % Perform the prediction step
+        [covarEst,uEst] = pred_step(uPrev,covarPrev,sampledData(i).omg,sampledData(i).acc,dt);
+        % Perform the update step
+        [uCurr,covar_curr] = upd_step(Z(:,i),covarEst,uEst);
+        % Store updated state for plotting
+        savedStates(:, i) = uCurr;
+        % Update previous values
+        uPrev = uCurr;
+        covarPrev = covar_curr;
+    end
+    % MY IMPLEMENTATION END ---------------------------------------------------
 end
-
-
-
-%plotData(savedStates, sampledTime, sampledVicon, 1, datasetNum);
+plotData(savedStates, sampledTime, sampledVicon, 1, datasetNum);
